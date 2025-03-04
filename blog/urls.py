09dbe9b3b501161729eeb604e2f9a68735e500e4
progettoTesi <pierpaologentile644@gmail.com>
2025-03-04 16:04:56 +0100

    ho cambiato l'HTML per questo sito.

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+#adesso aggiungiamo una view chiamata post_list, quindi se una persona accede al link, verrà reindirizzato a views.post_list
+urlpatterns = [
+    path('', views.post_list, name = 'post_list'), #post_list è il nome della url che verrà usata per identificare la views
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#creo la prima vista
+def post_list(request):
+    return render(request, 'blog/post_list.html', {}) #Quando un utente visita una pagina associata a questa view, 
+                                                      #Django carica e mostra il file blog/post_list.html.
