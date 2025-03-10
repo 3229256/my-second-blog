e0077fba237fa23a55cf870b0e31f385673ff7e0
progettoTesi <pierpaologentile644@gmail.com>
2025-03-10 12:30:51 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 #adesso aggiungiamo una view chiamata post_list, quindi se una persona accede al link, verrà reindirizzato a views.post_list
 urlpatterns = [
     path('', views.post_list, name = 'post_list'), #post_list è il nome della url che verrà usata per identificare la views
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
